797ce6194c539a2db1830668fe51c4764c8a0626
Matvey Pakhomov <matwick1@yandex.ru>
2022-01-25 12:53:35 +0400

    fix: update cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,12 +7,13 @@ const allowedCors = [
 
 // eslint-disable-next-line consistent-return
 function corsRequest(req, res, next) {
+  console.info(allowedCors);
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", allowedCors);
   }
 
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
